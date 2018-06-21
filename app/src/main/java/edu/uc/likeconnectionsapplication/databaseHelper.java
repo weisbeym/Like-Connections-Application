ebd2a8b66c4fb741314c7264630aedef7cfc0712
abellza <abellza@mail.uc.edu>
2018-06-21 10:13:29 -0400

    SQLite implemented
    Need object for user input

---
+++ app/src/main/java/edu/uc/likeconnectionsapplication/databaseHelper.java
@@ -0,0 +1,29 @@
+package edu.uc.likeconnectionsapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class databaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "profile.db";
+    public static final String TABLE_NAME ="profile_table";
+    public static final String COL_1 ="ID";
+    public static final String COL_2 ="firstName";
+    public static final String COL_3 ="lastName";
+
+    public databaseHelper( Context context ) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase(); //creates database
+    }
+
+    @Override
+    public void onCreate( SQLiteDatabase db ) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT)");
+    }
+
+    @Override
+    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
