6770a2e45c56a96e92055b0cf985a9fdb906dbb6
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-07-04 14:49:21 -0400

    started fleshing out the search service interface

--- app/src/main/java/edu/uc/likeconnectionsapplication/services/ISearchService.java
+++ app/src/main/java/edu/uc/likeconnectionsapplication/services/ISearchService.java
@@ -1,5 +1,7 @@
 package edu.uc.likeconnectionsapplication.services;
 
+import edu.uc.likeconnectionsapplication.dto.User;
+
 /**
  * search functionality for app
  * allows for users to search  based on interest object fields
@@ -7,4 +9,18 @@ package edu.uc.likeconnectionsapplication.services;
 
 public interface ISearchService {
 
+    /**
+     * @param userName the name of user being searched for
+     * @return the user object the userName is associated with
+     * @throws Exception if there in an error or the user does not exist.
+     */
+    public User getUserByUserName(String userName) throws Exception;
+
+    /**
+     * @param interest a string representing a specific interest or a field in one.
+     * Example: can search for football or sports(a category, see the javadoc in the Interest dto)
+     * @return users with that interest in their HashSet
+     * @throws Exception when there is no such interest, no users with that interest, or an error occurs.
+     */
+    public User getUserByInterest(String interest) throws Exception;
 }
