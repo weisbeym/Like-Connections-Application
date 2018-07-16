8d757c754e8c40b7f52b09002159826f7d8686c7
vincenjg <39882864+vincenjg@users.noreply.github.com>
2018-07-16 16:52:52 -0400

    vincenjg_commit2

--- app/src/main/java/edu/uc/likeconnectionsapplication/MainActivity.java
+++ app/src/main/java/edu/uc/likeconnectionsapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.uc.likeconnectionsapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,16 +10,86 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import edu.uc.likeconnectionsapplication.dao.databaseHelper;
 
 public class MainActivity extends AppCompatActivity {
     databaseHelper myDB;
+
+    public Button btnRegister;
+    public Button buttonSearch;
+
+
+//    public void register(View view){
+//        Intent goReg = new Intent(this, Register.class);
+//        startActivity(goReg);
+//    }
+
+    public void goRegister(){
+
+    buttonSearch = (Button)findViewById(R.id.buttonSearch);
+    buttonSearch.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent goRegister = new Intent(MainActivity.this, Register.class);
+            startActivity(goRegister);
+        }
+    });
+}
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        //Currently shows toasts for clicking anything on options menu
+
+        if(item.getItemId() == R.id.menuSearch){
+            Toast.makeText(MainActivity.this, "You've Clicked Search", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.menuProfile){
+            Toast.makeText(MainActivity.this, "You've Clicked Profile", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.menuAbout){
+            Toast.makeText(MainActivity.this, "You've Clicked About", Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == R.id.menuRegister){
+            Intent register = new Intent(MainActivity.this, Register.class);
+            startActivity(register);
+            return false;
+        }
+        if (item.getItemId() == R.id.menuProfile){
+            Intent profile = new Intent(MainActivity.this, Profile.class);
+            startActivity(profile);
+            return false;
+        }
+
+        if (item.getItemId() == R.id.menuSignin){
+            Intent signin = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(signin);
+            return false;
+        }
+
+
+
+        if(item.getItemId() == R.id.menuSettings){
+            Toast.makeText(MainActivity.this, "You've Clicked Settings", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+
+
+
+    }
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         myDB = new databaseHelper(this);
@@ -31,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        goRegister();
     }
     /*
     we inflate the menu in here, resource file
@@ -42,25 +114,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //Currently shows toasts for clicking anything on options menu
 
-        if(item.getItemId() == R.id.menuSearch){
-            Toast.makeText(MainActivity.this, "You've Clicked Search", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId() == R.id.menuProfile){
-            Toast.makeText(MainActivity.this, "You've Clicked Profile", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId() == R.id.menuAbout){
-            Toast.makeText(MainActivity.this, "You've Clicked About", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId() == R.id.menuRegister){
-            Toast.makeText(MainActivity.this, "You've Clicked Register", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId() == R.id.menuSettings){
-            Toast.makeText(MainActivity.this, "You've Clicked Settings", Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
+
 }
