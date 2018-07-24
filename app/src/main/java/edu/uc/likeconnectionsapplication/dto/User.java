892329225fb4f91871a0889ecb97c80874c4433a
abellza <abellza@mail.uc.edu>
2018-07-24 00:10:50 -0400

    Database rework
    Using 'Room' now. Created Interface for DB.

---
+++ app/src/main/java/edu/uc/likeconnectionsapplication/dto/User.java
@@ -0,0 +1,44 @@
+package edu.uc.likeconnectionsapplication.dto;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setUid( int uid ) {
+        this.uid = uid;
+    }
+
+    public void setFirstName( String firstName ) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName( String lastName ) {
+        this.lastName = lastName;
+    }
+
+    // Getters and setters are ignored for brevity,
+    // but they're required for Room to work.
+}
